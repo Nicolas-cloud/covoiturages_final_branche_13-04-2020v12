5fd3be75762f7b1f2cfe36c3a72ad6aa637b7df2
unknown <lefa.nicolas@gmail.com>
2020-03-19 16:05:24 +0100

    okk

--- src/Controller/TrajetController.php
+++ src/Controller/TrajetController.php
@@ -6,17 +6,29 @@ use App\Entity\Trajet;
 use App\Entity\User;
 use App\Repository\TrajetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\TrajetType;
-use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 
 class TrajetController extends AbstractController
 {
+    /*
+    private $entityManager;
+    */
+    /**
+     * Constructeur
+     */
+    /*
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }*/
+
     /**
      * Lister tous les trajets non expirés.
      * @Route("/trajet/", name="trajet.list")
@@ -57,16 +69,16 @@ class TrajetController extends AbstractController
      * @IsGranted("ROLE_USER")
      * @Route("/nouveau-trajet", name="trajet.create")
      * @param Request $request
-     * @param EntityManagerInterface $em
      * @return RedirectResponse|Response
      */
-    public function create(Request $request, EntityManagerInterface $em) : Response {
+    public function create(Request $request) : Response {
         $trajet = new Trajet();
         $form = $this->createForm(TrajetType::class, $trajet);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($trajet);
-            $em->ﬂush();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($trajet);
+            $entityManager->ﬂush();
             return $this->redirectToRoute('trajet.list');
         }
         return $this->render('trajet/create.html.twig', [
@@ -102,20 +114,20 @@ class TrajetController extends AbstractController
      * @param Trajet $trajet
      * @param EntityManagerInterface $em
      * @return Response
-     */
-    public function delete(Request $request, Trajet $trajet, EntityManagerInterface $em) : Response {
+    */
+    public function delete(Request $request, Trajet $trajet,  EntityManagerInterface $em) : Response {
         $form = $this->createFormBuilder()
             ->setAction($this->generateUrl('trajet.delete', ['slug' => $trajet->getSlug()]))
             ->getForm();
+
         $form->handleRequest($request);
         if ( ! $form->isSubmitted() || ! $form->isValid()) {
-
             return $this->render('trajet/delete.html.twig', [
             'trajet' => $trajet,
             'form' => $form->createView(),
-        ]);
+          ]);
         }
-
+        
         $em = $this->getDoctrine()->getManager();
         $em->remove($trajet);
         $em->ﬂush();
