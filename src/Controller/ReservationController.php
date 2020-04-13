429e9abe6abfdfdb0d22fbc79b31212dc3c1c47e
unknown <lefa.nicolas@gmail.com>
2020-04-13 17:58:18 +0200

    reservation

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -12,6 +12,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use App\Entity\Trajet;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
 
 /**
  * @Route("/{_locale}")
@@ -36,27 +38,39 @@ class ReservationController extends AbstractController
      * @param EntityManagerInterface $em
      * @return RedirectResponse|Response
      */
-    public function reservation(Request $request, Trajet $trajet, EntityManagerInterface $em): Response
+    public function reservation(Request $request, Trajet $trajet, EntityManagerInterface $em,  TranslatorInterface $translator): Response
     {
         $reservation= new Reservation();
 
-        $form = $this->createForm(ReservationType::class);
+        $form = $this->createForm(ReservationType::class, $reservation);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid())
         {
             $em->persist($reservation);
-            $trajet->addReservation($reservation);
             $reservation->setPassager($trajet);
             $place_dispo = $trajet->getNbPlaces();
             $place_reserv = $reservation->getNbPlaces();
+            if($place_dispo - $place_reserv < 0 ){
+                $error = "Vous ne pouvez pas réserver plus de places qu'il n'y a de places diponibles";
+                return $this->render('reservation/create.html.twig', array(
+                    'form' => $form->createView(),
+                    'reservation'=> $reservation,
+                    'trajet' => $trajet,
+                    'error' => $error,
+                ));
+
+            }
+            $error=" ";
             $trajet->setNbPlaces($place_dispo - $place_reserv);
             $em->flush();
-            return $this->redirectToRoute('/');
-        }           
+            return $this->redirectToRoute('trajet.list');
+        }    
+        $error=" ";
         return $this->render('reservation/create.html.twig', array(
             'form' => $form->createView(),
             'reservation'=> $reservation,
             'trajet' => $trajet,
+            'error' => $error,
         ));
     
     }
